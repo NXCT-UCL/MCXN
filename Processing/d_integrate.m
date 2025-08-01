4d88d85a50c95ae427690dd756cf11136172a455
ReconMCXN <ReconMCXN@CW05B24.thecrick.org>
2025-08-01 16:36:30 +0100

    relative path

--- Processing/d_integrate.m
+++ Processing/d_integrate.m
@@ -1,8 +1,8 @@
 clear;
 
-sampleFolder = 'D:\25_04_07\8_MuscleGFPD9_40kv_1p2um\';
-inFolder = strcat(sampleFolder,'corrected_dyno\');
-outFolder = strcat(sampleFolder, 'phase1000_dyno_filt\');
+sampleFolder = '..\';
+inFolder = strcat(sampleFolder,'corrected_filt2\');
+outFolder = strcat(sampleFolder, 'phase_filt2\');
 if ~exist(outFolder)
     mkdir(outFolder);
     disp(['Folder ', outFolder, ' created successfully.']);
@@ -15,20 +15,20 @@ im_names = dir([inFolder,'*.tif']);
 num_proj = length(im_names);
 
 bin_fact = 1;
-lx = 4096;
-ly = 4096;
+lx = 1440;
+ly = 1440;
 
-E = 12;
-R1 = 4.0769;
-R2 = 64.5785-R1;
+E = 15;
+R1 = 9.7571;
+R2 = 33.3470-R1;
 gamma = 1000;
-px = 8e-3;
+px = 4.5e-3;
 
 parfor idx = 1:num_proj
 
     fname = strcat(inFolder,im_names(idx).name);
     corrected = double(imread(fname));
-    sample = double(loadRawImage(fname));
+    sample = double(imread(fname));
   
     phase = tie_hom(corrected, E, R1, R2, gamma, px);
 
